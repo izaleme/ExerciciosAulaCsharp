823848eb838b9068e9b8e39b95b2568997aa6476
izabela <izabela.leme@plasutil.com.br>
2023-03-03 09:27:58 -0300

    Add DateTime, TimeSpan and DateTimeKind

--- 11Time/Program.cs
+++ 11Time/Program.cs
@@ -7,10 +7,12 @@ namespace _11Time
     {
         static void Main(string[] args)
         {
-
             // NO TIME IRMÃO
             // DateTime representa um instante
             // TimeSpan representa uma duração na forma de ticks (100 nanosegundos)
+            // DateTimeKind define três valores possíveis para a localidade da data: local (fuso horário do sistema), UTC (fuso horário GMT) e Unspecified
+
+            #region DateTime
 
             DateTime d1 = new DateTime(2000, 03, 18);
             DateTime d2 = new DateTime(2000, 03, 18, 12, 10, 5);
@@ -28,15 +30,8 @@ namespace _11Time
             DateTime d11 = DateTime.ParseExact("2000-03-18", "yyyy-MM-dd", CultureInfo.InvariantCulture);
             DateTime d12 = DateTime.ParseExact("18/03/2000 14:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
 
-            TimeSpan t1 = new TimeSpan();
-            TimeSpan t2 = new TimeSpan(900000000L);
-            TimeSpan t3 = new TimeSpan(0, 1, 30);
-            TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
-            TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321);
-            //TimeSpan t6 = new TimeSpan.FromDays(1.5);         => TimeSpan.From não está sendo reconhecido pelo VS
-
+            Console.WriteLine("**************");
             Console.WriteLine("** DateTime **");
-
             Console.WriteLine(d1);
             Console.WriteLine(d2);
             Console.WriteLine(d3);
@@ -49,16 +44,6 @@ namespace _11Time
             Console.WriteLine(d10);
             Console.WriteLine(d11);
             Console.WriteLine(d12);
-
-            Console.WriteLine();
-            Console.WriteLine("** TimeSpan **");
-
-            Console.WriteLine(t1);
-            Console.WriteLine(t2);
-            Console.WriteLine(t3);
-            Console.WriteLine(t3.Ticks);
-            Console.WriteLine(t4);
-            Console.WriteLine(t5);
 
             Console.WriteLine();
             Console.WriteLine("** Propriedades do DateTime **");
@@ -98,11 +83,74 @@ namespace _11Time
             DateTime y = x.Subtract(TimeSpan);
             TimeSpan t = x.Subtract(DateTime); */
 
-            Console.WriteLine();
-            Console.WriteLine("** Propriedades e Operações com TimeSpan **");
-            Console.WriteLine("test");
+            #endregion
+
+            #region TimeSpan
+
+            TimeSpan t1 = new TimeSpan();
+            TimeSpan t2 = new TimeSpan(900000000L);
+            TimeSpan t3 = new TimeSpan(0, 1, 30);
+            TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321);
+            TimeSpan t6 = TimeSpan.FromDays(1.5);
 
             Console.WriteLine();
+            Console.WriteLine("**************");
+            Console.WriteLine("** TimeSpan **");
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t3.Ticks);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+            Console.WriteLine(t6);
+
+            Console.WriteLine();
+            Console.WriteLine("** Propriedades e Operações com TimeSpan **");
+
+            TimeSpan t = new TimeSpan(2, 3, 5, 7, 11);
+            TimeSpan ts1 = new TimeSpan(1, 30, 10);
+            TimeSpan ts2 = new TimeSpan(0, 10, 5);
+
+            TimeSpan sum = ts1.Add(ts2);
+            TimeSpan sub = t1.Subtract(ts2);
+
+            Console.WriteLine(t);
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Subsract: " + sub);
+
+            Console.WriteLine("Days: " + t.Days);
+            Console.WriteLine("Hours: " + t.Hours);
+            Console.WriteLine("Minutes: " + t.Minutes);
+            Console.WriteLine("Milliseconds: " + t.Milliseconds);
+            Console.WriteLine("Seconds: " + t.Seconds);
+            Console.WriteLine("Ticks: " + t.Ticks);
+
+            Console.WriteLine("Total Days: " + t.TotalDays);
+            Console.WriteLine("Total Hours: " + t.TotalHours);
+            Console.WriteLine("Total Minutes: " + t.TotalMinutes);
+            Console.WriteLine("Total Seconds: " + t.TotalSeconds);
+            Console.WriteLine("Total Milliseconds: " + t.TotalMilliseconds);
+
+            #endregion
+
+            #region DateTimeKind
+
+            // Para converter um DateTime para local ou UTC: myDate.ToLocalTime() ou myDate.ToUniversalTime()
+            DateTime date1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            DateTime date2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            DateTime date3 = new DateTime(2000, 8, 15, 13, 5, 58);  // Unspecified
+
+            Console.WriteLine();
+            Console.WriteLine("******************");
+            Console.WriteLine("** DateTimeKind **");
+            Console.WriteLine("date1: " + date1);
+            Console.WriteLine("date1 Kind: " + date1.Kind);
+            Console.WriteLine("date1 to local: " + date1.ToLocalTime());
+            Console.WriteLine("date1 to utc: " + date1.ToUniversalTime());
+
+            #endregion
         }
     }
 }
