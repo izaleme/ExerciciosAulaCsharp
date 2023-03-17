7967d9b12fcb59e7b5aecee0096e42a369816cee
izabela <izabela.leme@plasutil.com.br>
2023-03-17 11:41:46 -0300

    Adding a example for enums and composition

---
+++ Course13Enums/Entities/Department.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course13Enums.Entities
+{
+    class Department
+    {
+        #region Atributos/Properties
+
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Construtores
+
+        public Department() { }
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+        #endregion
+    }
+}
