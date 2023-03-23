da6d88bfe8256262e4295aee5c5076e73eb33216
izabela <izabela.leme@plasutil.com.br>
2023-03-23 15:53:58 -0300

    add upcasting and downcasting examples

---
+++ Couse14Heranca/Entities/Account.cs
@@ -0,0 +1,42 @@
+﻿namespace Couse14Heranca.Entities
+{
+    class Account
+    {
+        #region  Attributes/Properties
+
+        public int Number { get; private set; }
+        public string Holder { get; private set; }  // private só permite alteração na classe atual
+        public double Balance { get; protected set; }   // protected só permite alteração desta classe ou de subclasses desta
+
+        #endregion
+
+        #region  Builders
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        #endregion
+
+        #region  Methods
+
+
+
+        #endregion
+    }
+}
---
+++ Couse14Heranca/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Couse14Heranca.Entities;
+
+namespace Couse14Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Ash", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Izabela", 0.0, 500.0);
+
+            // UPCASTING = conversão da subclasse para a superclasse
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+            // DOWNCASTING = conversão da superclasse para a subclasse
+            // O downcasting deve ser usado somente quando necessário
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            if (acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;     // <- dessa forma, ou:
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();   // usando downcasting para conseguir usar o UpdateBalance() do SavingsAccount
+                Console.WriteLine("Update!");
+            }
+        }
+    }
+}
