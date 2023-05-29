0973fa348a820e7d0364eb557673421795ff81fa
Izabela Leme <izaleme00@gmail.com>
2023-05-29 15:50:46 -0300

    + StreamWriter

--- 16Streams/Program.cs
+++ 16Streams/Program.cs
@@ -7,17 +7,44 @@ namespace Course16Streams
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\izabela\Documents\Ash\Curso Csharp Udemy\Projetos Estudo\file1.txt";
+            #region StreamReader e Using
+
+            //string path = @"C:\Users\izabela\Documents\Ash\Curso Csharp Udemy\Projetos Estudo\file1.txt";
+
+            //try
+            //{
+            //    // O bloco using automaticamente fecha os arquivos que ele abrir
+            //    using (StreamReader sr = File.OpenText(path))
+            //    {
+            //        while (!sr.EndOfStream)     // Enquanto não estiver no fim do arquivo
+            //        {
+            //            string line = sr.ReadLine();    // lê uma linha
+            //            Console.WriteLine(line);    // mostra a linha
+            //        }
+            //    }
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("An error occurred.");
+            //    Console.WriteLine(e.Message);
+            //}
+
+            #endregion
+
+            #region StreamWriter
+
+            string sourcePath = @"C:\Users\izabela\Documents\Ash\Curso Csharp Udemy\Projetos Estudo\file1.txt"; // Origem
+            string targetPath = @"C:\Users\izabela\Documents\Ash\Curso Csharp Udemy\Projetos Estudo\file2.txt"; // Destino
 
             try
             {
-                // O bloco using automaticamente fecha os arquivos que ele abrir
-                using (StreamReader sr = File.OpenText(path))
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
                 {
-                    while (!sr.EndOfStream)     // Enquanto não estiver no fim do arquivo
+                    foreach (string line in lines)
                     {
-                        string line = sr.ReadLine();    // lê uma linha
-                        Console.WriteLine(line);    // mostra a linha
+                        sw.WriteLine(line.ToUpper());   // Escreve a linha em caixa alta
                     }
                 }
             }
@@ -26,6 +53,9 @@ namespace Course16Streams
                 Console.WriteLine("An error occurred.");
                 Console.WriteLine(e.Message);
             }
+
+            #endregion
+
         }
     }
 }
