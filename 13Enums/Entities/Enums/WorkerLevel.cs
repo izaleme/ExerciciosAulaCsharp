3c5b281748a4acdaf390cc396ea97aa7a9102933
Izabela Leme <izabela.leme@plasutil.com.br>
2023-05-29 09:52:48 -0300

    Solving version errors

---
+++ 13Enums/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Course13Enums.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senhior = 2
+    }
+}
---
+++ 14Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,33 @@
+﻿namespace Couse14Heranca.Entities
+{
+    class BusinessAccount : Account
+    {
+        #region  Attributes/Properties
+
+        public double LoanLimit { get; set; }
+
+        #endregion
+
+        #region  Builders
+
+        public BusinessAccount() { }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance) // reaproveitamento o código do construtor da classe account
+        {
+            LoanLimit = loanLimit;
+        }
+
+        #endregion
+
+        #region  Methods
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+                Balance += amount;
+        }
+
+        #endregion
+    }
+}
