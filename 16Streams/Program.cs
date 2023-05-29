d48d79645a1f1dc1bb015621eaad69af6b57c844
Izabela Leme <izaleme00@gmail.com>
2023-05-29 15:44:16 -0300

    code optimization and "using" block

--- 16Streams/Program.cs
+++ 16Streams/Program.cs
@@ -9,32 +9,22 @@ namespace Course16Streams
         {
             string path = @"C:\Users\izabela\Documents\Ash\Curso Csharp Udemy\Projetos Estudo\file1.txt";
 
-            //FileStream fs = null;   // Pode ler ou escrever
-            StreamReader sr = null; // Lê uma stream binária
-
             try
             {
-                //fs = new FileStream(path, FileMode.Open);   // Abre o arquivo
-                //sr = new StreamReader(fs);   // Lê o arquivo
-
-                sr = File.OpenText(path);   // Faz o mesmo que o FileStream e StreamReader juntos
-
-                while (!sr.EndOfStream) // Enquanto não estiver no fim do arquivo
+                // O bloco using automaticamente fecha os arquivos que ele abrir
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    string line = sr.ReadLine();    // lê uma linha
-                    Console.WriteLine(line);    // mostra a linha
+                    while (!sr.EndOfStream)     // Enquanto não estiver no fim do arquivo
+                    {
+                        string line = sr.ReadLine();    // lê uma linha
+                        Console.WriteLine(line);    // mostra a linha
+                    }
                 }
             }
             catch (IOException e)
             {
                 Console.WriteLine("An error occurred.");
                 Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                // Independente se der erro ou não, vai fechar os arquivos
-                if (sr != null) sr.Close();
-                //if (fs != null) fs.Close();
             }
         }
     }
