9efdf3a6de752688226e2f10347d00a83c67e690
Izabela Leme <izaleme00@gmail.com>
2023-06-01 10:04:45 -0300

    service layer design (without interface)

---
+++ 18Interfaces/Entities/CarRental.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Course18Interfaces.Entities
+{
+    class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null;     // objetos não associados já recebem nulo, coloquei apenas pra aprender
+        }
+    }
+}
