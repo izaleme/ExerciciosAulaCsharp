25c0b72e2510a73e6216141c3515fdc92984e14a
Izabela Leme <izaleme00@gmail.com>
2023-06-06 14:54:40 -0300

    Generics

---
+++ 21Generics/PrintService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Generics
+{
+    class PrintService
+    {
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        public void AddValue(int value)
+        {
+            if (_count == 10)   // Significa que a variável já está cheia
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public int First()
+        {
+            if (_count == 0)   // Significa que a variável está vazia
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");       // Imprime do 0 ao 8 (penúltimo valor)
+            }
+
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);     // Imprime o último valor (posição 9)
+            }
+
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ 21Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());      // lê o valor passado
+                printService.AddValue(x);   // adiciona o valor na lista
+            }
+
+            printService.Print();   // imprime os elementos
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
