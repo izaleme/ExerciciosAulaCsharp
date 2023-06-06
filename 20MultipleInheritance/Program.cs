2d84639eca940066136092603ead1cf04704c905
Izabela Leme <izaleme00@gmail.com>
2023-06-06 11:19:36 -0300

    better code visualization (diamond problem solved)

--- 20MultipleInheritance/Program.cs
+++ 20MultipleInheritance/Program.cs
@@ -7,18 +7,26 @@ namespace MultipleInheritance
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("------------------------------------");
+
             Printer p = new Printer() { SerialNumber = 1080 };
             p.ProcessDoc("My letter");
             p.Print("My letter");
+
+            Console.WriteLine();
 
             Scanner s = new Scanner() { SerialNumber = 2003 };
             s.ProcessDoc("My email");
             Console.WriteLine(s.Scan());
 
+            Console.WriteLine();
+
             ComboDevice c = new ComboDevice() { SerialNumber = 3921 };
             c.ProcessDoc("My dissertation");
             c.Print("My dissertation");
-            Console.WriteLine(c.Scan()); ;
+            Console.WriteLine(c.Scan());
+
+            Console.WriteLine("------------------------------------");
         }
     }
 }
