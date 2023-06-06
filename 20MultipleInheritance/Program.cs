32877d54fec9cda5c395ae71ded98f4556531854
Izabela Leme <izaleme00@gmail.com>
2023-06-06 11:05:43 -0300

    Problema do diamante (herança múltipla)

---
+++ 20MultipleInheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MultipleInheritance.Devices;
+
+namespace MultipleInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new Printer() { SerialNumber = 1080 };
+            p.ProcessDoc("My letter");
+            p.Print("My letter");
+
+            Scanner s = new Scanner() { SerialNumber = 2003 };
+            s.ProcessDoc("My email");
+            Console.WriteLine(s.Scan());
+        }
+    }
+}
