716f320676dc605a6cb0ab01cde8d2a13650d172
Izabela Leme <izaleme00@gmail.com>
2023-06-16 10:53:58 -0300

    Added string extensions

--- 24Extensions/Program.cs
+++ 24Extensions/Program.cs
@@ -6,10 +6,11 @@ namespace Extensions
     {
         static void Main(string[] args)
         {
-            DateTime dt = new DateTime(2023, 05, 16, 8, 10, 45);    // 30 dias atrás
+            DateTime dt = new DateTime(2023, 05, 16, 10, 52, 45);    // Mostra o tempo passado desde a data (no caso 2023/05/16 = 30 dias atrás)
             Console.WriteLine(dt.ElapsedTime());
 
-
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));      // Corta 10 caracteres da string
         }
     }
 }
