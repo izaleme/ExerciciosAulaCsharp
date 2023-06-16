e7eb77c598d4fd2635cc8e24fd3cf668e350953b
Izabela Leme <izaleme00@gmail.com>
2023-06-16 10:43:52 -0300

    Criando um extension de DateTime

---
+++ 24Extensions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Extensions.Extensions;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 05, 16, 8, 10, 45);    // 30 dias atrás
+            Console.WriteLine(dt.ElapsedTime());
+
+
+        }
+    }
+}
